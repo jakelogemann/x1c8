302e9a654e29fc297f83774e971908fd5e9a1bc3
Jake Logemann <jlogemann@digitalocean.com>
2022-11-11 17:19:03 -0500

    experimental rewrite

--- pkg/xdg-extra/default.nix
+++ pkg/xdg-extra/default.nix
@@ -1,20 +1,38 @@
-{
-  symlinkJoin,
-  lib,
-  makeDesktopItem,
-}:
-with (builtins.fromTOML (builtins.readFile ./xdg-items.toml));
-  symlinkJoin {
-    name = "xdg-extra";
-    paths = lib.concatLists [
-      (builtins.map (entry:
-        makeDesktopItem {
-          name = "internal-${entry}";
-          desktopName = "Open ${entry}.internal.";
-          exec = "xdg-open https://${entry}.internal.digitalocean.com";
-        })
-      internalHostnames)
+{ stdenv }:
 
-      (builtins.map (entry: makeDesktopItem entry) desktopItem)
+stdenv.mkDerivation rec {
+  name = "xdg-extra";
+  src = ./xdg-items.toml;
+  builder = let
+    cfg = builtins.fromTOML (builtins.readFile src);
+    joinLines = builtins.concatStringsSep "\n";
+    joinLists = builtins.concatMap joinLines;
+    asKeyVal = builtins.mapAttrs (name: value: "${name}=${value}");
+
+    makeDesktopItem = {
+      Name,
+      ...
+
+    }@args: builtins.toFile "${Name}.desktop" (builtins.concatLists [
+      ["[Desktop Entry]"]
+      (builtins.attrValues (asKeyVal args))
+    ]);
+
+    desktopItems = builtins.concatLists [
+      cfg.desktopItem
+
+      (builtins.map (entry: {
+          Name = "internal-${entry}";
+          DesktopName = "Open ${entry}.internal.";
+          Exec = "xdg-open https://${entry}.internal.digitalocean.com";
+        })
+        cfg.internalHostnames)
     ];
-  }
+
+    contents = builtins.concatLists [
+      [ "source $stdenv/setup" "mkdir -vp $out/share/applications" ]
+      (builtins.map (i: "cp ${makeDesktopItem i} $out/share/applications/") desktopItems)
+    ];
+  in
+    builtins.toFile "builder.sh" (joinLines contents);
+}
