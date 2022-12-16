bb4fccdc9ada47ed89262cda62366d3a14b55eeb
Jake Logemann <jlogemann@digitalocean.com>
2022-12-16 17:37:35 -0500

    fix neovim

--- user.nix
+++ user.nix
@@ -37,6 +37,131 @@ in {
         runtimeInputs = [openldap];
         text = "ldapsearch -xLLLH ldaps://ldap-primary.internal.digitalocean.com -b ou=Groups,dc=internal,dc=digitalocean,dc=com";
       })
+
+      (neovim.override {
+        extraName = "-jlogemann";
+        viAlias = true;
+        vimAlias = true;
+        extraLuaPackages = p: [
+          p.luacheck
+          p.lua-lsp
+          p.nvim-client
+          p.nvim-cmp
+          p.serpent
+          p.vicious
+          p.luarocks
+          p.luarocks-nix
+        ];
+
+        configure.customRC = builtins.concatStringsSep "\n" [
+          "lua vim.env.PATH = vim.env.PATH .. \":${symlinkJoin {
+            name = "extra-tools";
+            paths = [
+              alejandra
+              bat
+              commitlint
+              deadnix
+              gitlint
+              gnumake
+              gnupg
+              gnused
+              gnutar
+              nix-linter
+              jq
+              yamllint
+              yaml-language-server
+              rnix-lsp
+              sumneko-lua-language-server
+              rust-analyzer
+              neovim
+              nodejs
+              ripgrep
+              shellcheck
+              shellharden
+              stylua
+            ];
+          }}/bin\""
+          "luafile ${./neovim.lua}"
+        ];
+
+        configure.packages.default = with vimPlugins; {
+          start = [
+            SchemaStore-nvim
+            aerial-nvim
+            rust-tools-nvim
+            alpha-nvim
+            better-escape-nvim
+            bufferline-nvim
+            catppuccin-nvim
+            cmp-buffer
+            cmp-calc
+            cmp-copilot
+            cmp-dictionary
+            cmp-digraphs
+            cmp-emoji
+            cmp-git
+            cmp-nvim-lsp
+            cmp-nvim-lua
+            cmp-omni
+            cmp-path
+            cmp-rg
+            cmp-spell
+            cmp-treesitter
+            colorizer
+            copilot-vim
+            crates-nvim
+            dressing-nvim
+            editorconfig-nvim
+            formatter-nvim
+            fzf-lua
+            fzf-vim
+            neodev-nvim
+            gitsigns-nvim
+            impatient-nvim
+            lspkind-nvim
+            lualine-nvim
+            luasnip
+            mini-nvim
+            neorg
+            neoscroll-nvim
+            nightfox-nvim
+            null-ls-nvim
+            nvim-autopairs
+            nvim-cmp
+            nvim-config-local
+            nvim-cursorline
+            nvim-dap
+            nvim-dap-ui
+            nvim-lspconfig
+            nvim-notify
+            nvim-spectre
+            nvim-surround
+            nvim-terminal-lua
+            nvim-treesitter-context
+            nvim-treesitter-refactor
+            nvim-treesitter-textobjects
+            nvim-treesitter.withAllGrammars
+            nvim-web-devicons
+            plenary-nvim
+            toggleterm-nvim
+            trouble-nvim
+            vim-automkdir
+            vim-caddyfile
+            vim-easy-align
+            vim-gnupg
+            vim-go
+            vim-lastplace
+            vim-nftables
+            vim-nix
+            vim-nixhash
+            vim-pager
+            vim-protobuf
+            which-key-nvim
+            # wilder-nvim
+            windows-nvim
+          ];
+        };
+      })
     ];
   };
   nix.settings.allowed-users = [login];
