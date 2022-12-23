422ef42c6b75fd4c552fb01a32c9828b62a52bb1
Jake Logemann <jlogemann@digitalocean.com>
2022-12-22 19:58:59 -0500

    expand system.py

--- system.py
+++ system.py
@@ -1,20 +1,86 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python3 -p python3 python3Packages.typer python3Packages.ipython
+#! nix-shell -i python3 -p python3 python3Packages.typer python3Packages.ipython python3Packages.rich
 # -*- coding: utf-8 -*-
 # ===================================================
 
+
 import os
+import json
 import pathlib
 import platform
 import subprocess
+import functools
+from pprint import pp
+from enum import Enum
+
+# smartly tell the user if they're missing typer. this is really
+# nice for bootstrapping new environments.
+try:
+    from typer import Typer
+except Exception as e:
+    print("Could not import third-party libraries... (are they installed?): ", e)
+    os.exit(-1)
 
 EDITOR = os.environ.get("EDITOR", "nvim")
 PAGER = os.environ.get("PAGER", "bat")
 USER = os.environ.get("USER", "nobody")
+HOST = os.environ.get("HOST", "laptop")
 SCRIPT = pathlib.Path(__file__).absolute()
 REPO = pathlib.Path(__file__).absolute().parent
 
+# prepare our command-line app's options.
+cli_opts = dict(no_args_is_help=True)
+cli_opts["rich_markup_mode"] = "rich"
+cli_opts["add_completion"] = False
+cli_opts["help"] = """Interface for managing {REPO}.""".format(**globals())
+cli_opts["epilog"] = " ".join([
+        "[italic]"
+        "I always said I'd replace myself \"with a small Bash script\" ..."
+        "I was [bold]so close[/bold]... its Python."
+        "[/italic]"
+        ])
 
+# define our command-line application.
+cli = Typer(**cli_opts)
+
+class Flake:
+    _inputs = None
+    _outputs = None
+
+    @staticmethod
+    def uri():
+        return "{}#{}".format(REPO, HOST)
+
+    @classmethod
+    def outputs(cls):
+        if cls._outputs is None:
+            cls._outputs = json.loads(subprocess.getoutput("nix flake metadata --quiet --json {}".format(REPO)))
+        return cls._outputs
+
+    @classmethod
+    def inputs(cls):
+        if cls._inputs is None:
+            cls._inputs = json.loads(subprocess.getoutput("nix flake show --quiet --json {}".format(REPO)))
+        return cls._inputs
+
+
+class NixOS:
+    class RebuildCommand(Enum):
+        Switch = "switch"
+        Build = "build"
+        DryActivate = "dry-activate"
+        DryBuild = "dry-build"
+        Test = "test"
+        Boot = "boot"
+        VmWithBootloader = "build-vm-with-bootloader"
+
+    @cli.command()
+    def rebuild(subcmd: RebuildCommand = "build"):
+        """Wraps `nixos-rebuild`."""
+
+        return subprocess.run(["nixos-rebuild", "--flake", NixFlake.uri(), subcmd.value])
+
+@cli.command()
 def deps():
     """
     prints the system dependencies as reported by nix.
@@ -22,13 +88,16 @@ def deps():
     return subprocess.call(["nix", "flake", "metadata"])
 
 
+@cli.command()
 def outputs():
     """
     prints the system outputs as reported by nix.
     """
-    return  subprocess.call(["nix", "flake", "show"])
+    s = _get_flake_stuff()
 
 
+
+@cli.command()
 def edit(file: str = str(SCRIPT), editor: str = EDITOR):
     """
     quickly open a given file with the default editor; or an editor you
@@ -37,6 +106,7 @@ def edit(file: str = str(SCRIPT), editor: str = EDITOR):
     subprocess.call([editor, file])
 
 
+@cli.command()
 def shell():
     """Open interactive IPython shell."""
     import IPython
@@ -101,34 +171,8 @@ def shell():
     return IPython.start_ipython(**ipy_args)
 
 
-def main():
-    """main entrypoint to the command-line application."""
-    # smartly tell the user if they're missing typer. this is really
-    # nice for bootstrapping new environments.
-    try:
-        from typer import Typer
-    except Exception as e:
-        print("Could not import typer (is it installed?): ", e)
-        return None
-    # prepare our command-line app's options.
-    cli_opts = dict(no_args_is_help=True)
-    cli_opts["rich_markup_mode"] = "rich"
-    cli_opts["add_completion"] = False
-    cli_opts["help"] = """Interface for managing {REPO}.""".format(**globals())
-    cli_opts["epilog"] = """
-    I always said I'd replace myself "with a small bash script";
-    I was so close. I should have known it'd be python...
-    """
-    # define our command-line application.
-    cli = Typer(**cli_opts)
-    # add all commands to the command-line application we defined.
-    for cmd in [deps, edit, shell, outputs]:
-        cli.command()(cmd)
+if __name__ == "__main__":
     # ensure the script is always run from the repo's directory.
     os.chdir(REPO)
     # finally, execute the finished command-line application.
-    return cli()
-
-
-if __name__ == "__main__":
-    main()
+    cli()
